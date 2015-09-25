dc394d886ced69e3e07b877b3285d7d116de8fdc
Eric Kramer <cosmocracy@yahoo.com>
2015-09-25 15:02:00 -0400

    Reworked to focus on disease group (collection protocol category)
    Rework of underlying data structures, UI, and page logic.  Prepping for
    HLD COG visit

--- HHOW-Visualizer.js
+++ HHOW-Visualizer.js
@@ -3,25 +3,18 @@ String.prototype.trunc =
           return this.substr(0,n-1)+(this.length>n?'&hellip;':'');
       };
 
-d3.csv("StudyList.csv", function(error,data) {
-	if (error) {
-		var msg = "Could not read study list";
-		console.log(msg);
-		alert(msg);
-		return;
-	}
-	data.forEach(function (d) {
-		if(d.StudyTitle.length > 0) {
-		$('#protocol-picker')
-			  .append($('<option data-subtext="'+d.StudyTitle.trunc(100)+'">', { value : d.CtepStudyID })
-			  .text(d.CtepStudyID));
-		}
-	});
-	$('#protocol-picker').selectpicker('refresh');
-});
+function remove_empty_bins(source_group) {
+    return {
+        all: function () {
+            return source_group.all().filter(function (d) {
+                return d.value != 0;
+            });
+        }
+    };
+}
 
 var displayDateFormat = d3.time.format("%m/%d/%Y");
-var filename = "SpecimenStats10000.csv";
+var filename = "SpecimenStats.csv";
 d3.csv(filename, function (error, data) {
     if (error) {
         var msg = "Could not read " + filename;
@@ -30,12 +23,7 @@ d3.csv(filename, function (error, data) {
         return;
     }
 
-    // CSV data pre-processing (e.g., type conversion, attribute extraction)
-    var dateFormat = d3.time.format("%Y-%m-%d");
     data.forEach(function (d) {
-        d.collectionDate = dateFormat.parse(d.collectionDate);
-        d.collectionMonth = d3.time.month(d.collectionDate); // pre-calculate month for better performance
-        // d.SpecimenCount = +d.SpecimenCount;
         var number = d.SpecimenCount.match(/\d+$/);
 		d.SpecimenCount = parseInt(number, 10);
     });
@@ -43,183 +31,130 @@ d3.csv(filename, function (error, data) {
     var ndx = crossfilter(data);
     var all = ndx.groupAll();
 
-    var collectionYearDimension = ndx.dimension(function (d) {
-        return d3.time.year(d.collectionDate).getFullYear();
-    });
-    var collectionYearGroup = collectionYearDimension.group();
+    var protocolCategoryDimension = ndx.dimension(function (d) { return d.ProtocolCategory; });
+    var protocolCategoryGroup = protocolCategoryDimension.group().reduceSum(function (d) { return d.SpecimenCount; });
+    var protocolCategoryChart = dc.pieChart('#protocol-category')
+        .dimension(protocolCategoryDimension)
+        .group(protocolCategoryGroup)
+        .width(600) // (optional) define chart width, :default = 200
+        .height(350) // (optional) define chart height, :default = 200
+        .radius(150)
+        .innerRadius(100)
+        .legend(dc.legend().x(0).y(0))//.gap(5))
+        .cx(400)
+        //.label(null)
+    .minAngleForLabel(100);
 
-    var collectionYearBucketedDimension = ndx.dimension(function (d) {
-        var year = d3.time.year(d.collectionDate).getFullYear();
-        return (year < 2005) ? 0 : year;
-        // return year;
-    });
-    var collectionYearBucketedGroup = collectionYearBucketedDimension.group();
+    var protocolStatusDimension = ndx.dimension(function (d) { return d.CollectionProtocolStatus; });
+    var protocolStatusGroup = protocolStatusDimension.group().reduceSum(function (d) { return d.SpecimenCount; });
+    var protocolStatusChart = dc.pieChart('#protocol-status')
+        .dimension(protocolStatusDimension)
+        .group(protocolStatusGroup)
+        .width(300) // (optional) define chart width, :default = 200
+        .height(200) // (optional) define chart height, :default = 200
+        .radius(50)
+        .cx(200)
+        .cy(50)
+        .legend(dc.legend().x(0).y(0))//.gap(5))
+        //.innerRadius(50)
+        .minAngleForLabel(100);
 
-    var collectionYearPerformanceGroup = collectionYearDimension.group().reduce(
-        /* callback for when data is added to the current filter results */
-        function (p, v) {
-            p.count += v.SpecimenCount;
-            return p;
-        },
-        /* callback for when data is removed from the current filter results */
-        function (p, v) {
-            p.count -= v.SpecimenCount;
-            return p;
-        },
-        /* initialize p */
-        function () {
-            return {
-                count: 0
-            };
-        }
-    );
-
+    var protocolDimension = ndx.dimension(function (d) { return d.CtepStudyID; });
+    var protocolGroup = protocolDimension.group().reduceSum(function (d) { return d.SpecimenCount; });
 /*
-    var protocolDimension = ndx.dimension(function (d) {
-        return d.CtepStudyID;
-    });
-*/
-    var collectionDateDimension = ndx.dimension(function (d) {
-        return d.collectionDate;
-    });
-
-    var collectionMonths = ndx.dimension(function (d) {
-        return d.collectionMonth;
-    });
-
-    // group by total movement within month
-    var collectionMonthsGroup = collectionMonths.group().reduceSum(function (d) {
-        return d.SpecimenCount; // ???
-    });
-
-    var specimensByMonthGroup = collectionMonths.group().reduceSum(function (d) {
-        return d.SpecimenCount; // / 500000;
-    });
-
-    var specimenBiophysicalType = ndx.dimension(function (d) {
-        return d.specimenBiophysicalType;
-    });
-    var specimenBiophysicalTypeGroup = specimenBiophysicalType.group();
-
-    var diseaseStatus = ndx.dimension(function (d) {
-        return d.diseaseStatus;
-    });
-    var diseaseStatuseGroup = diseaseStatus.group();
-
-    var pathologicalStatus = ndx.dimension(function (d) {
-        return d.pathologicalStatus;
-    });
-    var pathologicalStatusGroup = pathologicalStatus.group();
-
-    var specimenBiophysicalType = ndx.dimension(function (d) {
-        return d.specimenBiophysicalType;
-    });
-    var specimenBiophysicalTypeGroup = specimenBiophysicalType.group();
-
-    var collectionYearChart = dc.rowChart('#collection-year');
-    collectionYearChart
+    var protocolChart = dc.bubbleCloud('#protocols')
+        .dimension(protocolDimension)
+        .group(protocolGroup)
         .width(300)
-        .height(350)
-        .margins({
-            top: 20,
-            left: 10,
-            right: 10,
-            bottom: 20
-        })
-        .group(collectionYearBucketedGroup)
-        .dimension(collectionYearBucketedDimension)
-        /*
-        .ordinalColors(['#3182bd', '#6baed6', '#9ecae1', '#c6dbef', '#dadaeb'])
-        .label(function (d) {
-            return d.key.split('.')[1];
-        })
-        .title(function (d) {
-            return d.value;
-        })
-        */
-        .label(function (d) {
-            return d.key == 0 ? "Before 2005" : d.key;
-        })
+        .height(300) // (optional) define chart height, :default = 200
+        //.x(d3.scale.ordinal().domain([0,150000]))
+        .x(d3.scale.ordinal())
+        .r(d3.scale.linear().domain([0, 150000]))
+        .maxBubbleRelativeSize(0.1)
+        .radiusValueAccessor(function (d) {
+            return d.value; // 1; // d.value; // 0.5; //d.SpecimenCount;
+        });
+*/
+    var protocolGroup2 = remove_empty_bins(protocolGroup);
+/*
+    var protocolChart2 = dc.rowChart('#protocols2')
+        .dimension(protocolDimension)
+        .group(protocolGroup2)
+        .width(700)
+        .height(700)
+        .elasticX(true);
+*/
+    var protocolChart3 = dc.barChart('#protocols3')
+        .dimension(protocolDimension)
+        .group(protocolGroup2)
+        .width(1100)
+        .height(400)
+        .elasticY(true)
         .elasticX(true)
-        .xAxis().ticks(4);
+        .x(d3.scale.ordinal())
+        .xUnits(dc.units.ordinal)
+        .brushOn(false)
+        .margins({top:0, bottom:70, left:60, right:0})
+        .yAxisLabel("# Specimens")
+        .barPadding(0.1)
+        .outerPadding(0.05)
+        .renderlet(function (chart) {
+            chart.selectAll("g.x text")
+              .attr('dx', '-30')
+              .attr('dy', '-7')
+              .attr('transform', "rotate(-90)");
+        });
+        
 
-    var diseaseStatusChart = dc.pieChart('#disease-status');
-    diseaseStatusChart
+    var specimenBiophysicalType = ndx.dimension(function (d) { return d.specimenBiophysicalType; });
+    var specimenBiophysicalTypeGroup = specimenBiophysicalType.group().reduceSum(function (d) { return d.SpecimenCount; });
+    var specimenBiophysicalTypeChart = dc.pieChart('#specimen-biophysical-type')
+        .dimension(specimenBiophysicalType)
+        .group(specimenBiophysicalTypeGroup)
+/*
+        .width(300) // (optional) define chart width, :default = 200
+        .height(300) // (optional) define chart height, :default = 200
+        .radius(150)
+        .innerRadius(20);
+*/
+        .width(300) // (optional) define chart width, :default = 200
+        .height(200) // (optional) define chart height, :default = 200
+        .radius(50)
+        .cx(200)
+        .cy(50)
+        .legend(dc.legend().x(0).y(0))//.gap(5))
+        //.innerRadius(50)
+        .minAngleForLabel(100);
+
+    var diseaseStatus = ndx.dimension(function (d) { return d.diseaseStatus; });
+    var diseaseStatuseGroup = diseaseStatus.group().reduceSum(function (d) { return d.SpecimenCount; });
+    var diseaseStatusChart = dc.pieChart('#disease-status')
         .dimension(diseaseStatus)
         .group(diseaseStatuseGroup)
         .width(300) // (optional) define chart width, :default = 200
-        .height(300) // (optional) define chart height, :default = 200
-        .radius(150)
-        .innerRadius(20);
-
-    var pathologicalStatusChart = dc.pieChart('#pathological-status');
-    pathologicalStatusChart
-        .dimension(pathologicalStatus)
-        .group(pathologicalStatusGroup)
-        .width(300) // (optional) define chart width, :default = 200
-        .height(300) // (optional) define chart height, :default = 200
-        .radius(150)
-        .innerRadius(20);
-
-    var specimenBiophysicalTypeChart = dc.pieChart('#specimen-biophysical-type');
-    specimenBiophysicalTypeChart
-        .dimension(specimenBiophysicalType)
-        .group(specimenBiophysicalTypeGroup)
-        .width(300) // (optional) define chart width, :default = 200
-        .height(300) // (optional) define chart height, :default = 200
-        .radius(150)
-        .innerRadius(20);
+        .height(200) // (optional) define chart height, :default = 200
+        .radius(50)
+        .cx(200)
+        .cy(50)
+        .legend(dc.legend().x(0).y(0))//.gap(5))
+        //.innerRadius(50)
+        .minAngleForLabel(100);
 
     dc.dataCount('.dc-data-count')
         .dimension(ndx)
         .group(all);
 
-    /*
-    specimenCountGroup = ndx.groupAll().reduce(
-        // callback for when data is added to the current filter results
-        function (p, v) {
-            p.count += v.SpecimenCount;
-            return p;
-        },
-        // callback for when data is removed from the current filter results
-        function (p, v) {
-            p.count -= v.SpecimenCount;
-            return p;
-        },
-        // initialize p
-        function () {
-            return {
-                count: 0
-            };
-        }
-    );
-    */
-
-    /*
-    //var specimenCounterND = dc.numberDisplay(".specimen-counter");
-    var specimenCounterND = dc.numberDisplay(".specimen-counter");
-    specimenCounterND
-        .formatNumber(d3.format(".3s"))
-        .valueAccessor(function (p) {
-            return p.count;
-        })
-        .group(specimenCountGroup);
-    */
-
-    var specimenCountGroup = ndx.groupAll().reduceSum(function (p) {
-			return p.SpecimenCount; });
+    var specimenCountGroup = ndx.groupAll().reduceSum(function (p) { return p.SpecimenCount; });
     var specimenCountND = dc.numberDisplay(".candidate-specimens-counter")
         .formatNumber(d3.format(".g"))
-        .valueAccessor(function (p) {
-			return p; })
+        .valueAccessor(function (p) { return p; })
         .group(specimenCountGroup)
         .transitionDuration(0);
 
     var totalSpecimenCount = specimenCountGroup.value();
     var specimenCountND = dc.numberDisplay(".total-specimens-counter")
         .formatNumber(d3.format(".g"))
-        .valueAccessor(function (p) {
-			return totalSpecimenCount; })
+        .valueAccessor(function (p) { return totalSpecimenCount; })
         .group(specimenCountGroup)
         .transitionDuration(0);
 
@@ -231,65 +166,29 @@ d3.csv(filename, function (error, data) {
     http://paletton.com/#uid=72G0u0kfDpf6bEGaJuhkgkIphfP
     http://stackoverflow.com/questions/27789872/display-the-number-of-distinct-items-with-data-count-widget
     */
-    var specimenStatsGrid = dc.dataTable('#specimen-stats-grid');
-    specimenStatsGrid
-        .width(960).height(800)
-        .dimension(collectionDateDimension)
-        .group(function (d) {
-            return "(Candidate Subset)"
-        })
+    var specimenStatsGrid = dc.dataTable('#specimen-stats-grid')
+        .width(960)
+        .height(800)
+        .dimension(protocolDimension)
+        .group(function (d) { return "(Candidate Subset)" })
         .size(20) // (optional) max number of records to be shown, :default = 25
         .columns([
-            function (d) { return displayDateFormat(d.collectionDate); },
+            function (d) { return d.CtepStudyID; },
+            function (d) { return d.CollectionProtocolStatus; },
+            function (d) { return d.ProtocolCategory; },
             function (d) { return d.specimenBiophysicalType; },
             function (d) { return d.preservationType; },
             function (d) { return d.fmaAnatomicSourceLocation; },
             function (d) { return d.pathologicalStatus; },
             function (d) { return d.diseaseStatus; },
-            function (d) { return d.collectionInstitution; },
-            function (d) { return d.CtepStudyID; },
             function (d) { return d.SpecimenCount; }
         ])
-        .sortBy(function (d) {
-            return d.collectionDate;
-        });
-
-    var monthlySpecimenCollectionChart = dc.barChart('#monthly-specimen-collection-chart');
-    monthlySpecimenCollectionChart
-    .width(900)
-    .height(100)
-    .margins({
-        top: 0,
-        right: 50,
-        bottom: 20,
-        left: 40
-    })
-    .dimension(collectionMonths)
-    .group(specimensByMonthGroup)
-    .centerBar(true)
-    .gap(1)
-    .x(d3.time.scale().domain([new Date(2000, 0, 1), new Date(2015, 11, 31)]))
-    .elasticY(true)
-    .round(d3.time.month.round)
-    // .alwaysUseRounding(true)
-    .xUnits(d3.time.months);
-    monthlySpecimenCollectionChart.yAxis().ticks(0);
-
-
+        .sortBy(function (d) { return d.CtepStudyID; });
 
     $(document).ready(function () {
 		// Render all charts
         dc.filterAll();
         dc.renderAll();
-        /*
-        // Install listener for protocol list changes
-        $('#protocol-picker').change( function() {
-			var selectedValues = $('#protocol-picker').val();
-			protocolDimension.filter(selectedValues);
-			dc.renderAll();
-			dc.redrawAll();
-		});
-		*/
     });
 });
 
--- SpecimenStats.sql
+++ SpecimenStats.sql
@@ -1,3 +1,10 @@
+
+....NOW UNUSED... Data comes directly from snapshot of RESRICDBDEV04.HHOW_DataVis_DEV.[dbo].[SpecimensV1]...
+
+
+
+
+
 -- Save as SpecimenStats20000.csv
 with SpecimenCollectionProtocolLookup as (
 	SELECT [SpecimenID], min(CtepStudyID) as CtepStudyID
@@ -53,6 +60,67 @@ group by
 	*/
 order by newid();
 
+
+-- NEW VERSION --
+
+use NavigatorMapping
+go
+
+with SpecimenCollectionProtocolLookup as (
+	SELECT [SpecimenID], min(CtepStudyID) as CtepStudyID
+	FROM [NavigatorMapping].[dbo].[StageNavigatorProtocolsFromBankDataWarehouse]
+	where IsCollectionProtocol=1
+	group by [SpecimenID], [ProtocolName]
+)
+create view SpecimensV1 as
+select top 10000
+	/* participantId,
+	Barcode,
+	currentAmount,
+	unitOfMeasure, */
+	cast(collectionDateTime as date) as collectionDate,
+	-- */
+	specimenBiophysicalType,
+	preservationType,
+	fmaAnatomicSourceLocation,
+	pathologicalStatus,
+	diseaseStatus,
+	-- collectionTimePoint,
+	org.name collectionInstitution,
+	SpecimenCollectionProtocolLookup.CtepStudyID,
+	case when Studies.ProtocolCategory like '%\%' 
+		then reverse(left(reverse(Studies.ProtocolCategory), charindex('\', reverse(Studies.ProtocolCategory)) -1)) 
+		else Studies.ProtocolCategory
+	end as ProtocolCategory,
+
+	/*
+	case
+		when Studies.CONDITION_NAME_SET = '||' then '(Unknown/Unmapped)'
+		else substring(Studies.CONDITION_NAME_SET,2,CHARINDEX('|',Studies.CONDITION_NAME_SET,2)-2) 
+	end Disease,
+	*/
+	count(distinct barcode) as SpecimenCount
+from [NavigatorMapping].[dbo].[StageNavigatorMappingFromBankDataWarehouse] stage
+	left outer join resc3prod.[CTEPIQ].[dbo].[Organizations] org on InstitutionID_CTEP = org.CTEP_ID
+	inner join SpecimenCollectionProtocolLookup on stage.bdwpk_SpecimenID = SpecimenCollectionProtocolLookup.SpecimenID
+	left outer join resc3prod.CTEPIQ.dbo.CTEP_COG_StudiesCategorized as [Studies] on Studies.CTEP_STUDY_ID = SpecimenCollectionProtocolLookup.CtepStudyID
+where
+	specimenAccessStatus = 1 and
+	collectionDateTime is not null and 
+	stage.STARS_StatisticalGroup='COG' and 
+	Studies.ProtocolCategory is not null
+group by
+	cast(collectionDateTime as date),
+	specimenBiophysicalType,
+	preservationType,
+	fmaAnatomicSourceLocation,
+	pathologicalStatus,
+	diseaseStatus,
+	org.name,
+	SpecimenCollectionProtocolLookup.CtepStudyID,
+	Studies.ProtocolCategory
+order by newid();
+
 /*
 SELECT distinct CtepStudyID
 FROM [NavigatorMapping].[dbo].[StageNavigatorProtocolsFromBankDataWarehouse]
@@ -72,4 +140,99 @@ where
 		FROM [NavigatorMapping].[dbo].[StageNavigatorProtocolsFromBankDataWarehouse]
 		where PrimarySponsorGroup = 'COG' and IsCollectionProtocol=1
 	)
+order by 1;
+
+select *
+FROM [NavigatorMapping].[dbo].[StageNavigatorProtocolsFromBankDataWarehouse]
+where PrimarySponsorGroup = 'COG' and IsCollectionProtocol=1
+
+with data as (
+	select ProtocolName, ProvisionalDiagnosis, count(*) as RecordCount
+	from BankDataWarehouse.dbo.SpecimenProtocols
+	where PrimarySponsorGroup='COG' and IsCollectionProtocol=1
+	group by ProtocolName, ProvisionalDiagnosis
+	--order by 3 desc
+	)
+select data.*, sum(recordcount) over (order by recordcount desc rows unbounded preceding) as running_total
+from data
+
+
+
+-- 
+-- NEW NEW VERSION in new database: RESRICDBDEV04 HHOW_DataVis_DEV
+--
+
+if object_id('dbo.SpecimensV1') is not null
+	drop view SpecimensV1;
+go
+
+create view SpecimensV1 as
+with SpecimenCollectionProtocolLookup as (
+	SELECT [SpecimenID], min(CtepStudyID) as CtepStudyID
+	FROM [NavigatorMapping].[dbo].[StageNavigatorProtocolsFromBankDataWarehouse]
+	where IsCollectionProtocol=1
+	group by [SpecimenID], [ProtocolName]
+)
+select -- top 10000
+	/* participantId,
+	Barcode,
+	currentAmount,
+	unitOfMeasure, */
+	cast(collectionDateTime as date) as collectionDate,
+	-- */
+	specimenBiophysicalType,
+	preservationType,
+	fmaAnatomicSourceLocation,
+	pathologicalStatus,
+	diseaseStatus,
+	-- collectionTimePoint,
+	org.name collectionInstitution,
+	SpecimenCollectionProtocolLookup.CtepStudyID,
+	Studies.ProtocolCategory,
+	/*
+	case when Studies.ProtocolCategory like '% \ %' 
+		then reverse(left(reverse(Studies.ProtocolCategory), charindex(' \ ', reverse(Studies.ProtocolCategory)) -1)) 
+		else Studies.ProtocolCategory
+	end as ProtocolCategory,
+	*/
+
+	/*
+	case
+		when Studies.CONDITION_NAME_SET = '||' then '(Unknown/Unmapped)'
+		else substring(Studies.CONDITION_NAME_SET,2,CHARINDEX('|',Studies.CONDITION_NAME_SET,2)-2) 
+	end Disease,
+	*/
+	count(distinct barcode) as SpecimenCount
+from [NavigatorMapping].[dbo].[StageNavigatorMappingFromBankDataWarehouse] stage
+	left outer join resc3prod.[CTEPIQ].[dbo].[Organizations] org on InstitutionID_CTEP = org.CTEP_ID
+	left outer join SpecimenCollectionProtocolLookup on stage.bdwpk_SpecimenID = SpecimenCollectionProtocolLookup.SpecimenID
+	left outer join resc3prod.CTEPIQ.dbo.CTEP_COG_StudiesCategorized as [Studies] on Studies.CTEP_STUDY_ID = SpecimenCollectionProtocolLookup.CtepStudyID
+where
+	specimenAccessStatus = 1 and
+	collectionDateTime is not null and 
+	stage.STARS_StatisticalGroup='COG' 
+	-- and Studies.ProtocolCategory is not null
+group by
+	cast(collectionDateTime as date),
+	specimenBiophysicalType,
+	preservationType,
+	fmaAnatomicSourceLocation,
+	pathologicalStatus,
+	diseaseStatus,
+	org.name,
+	SpecimenCollectionProtocolLookup.CtepStudyID,
+	Studies.ProtocolCategory;
+go
+
+if object_id('dbo.SpecimensV1_Snapshot') is not null
+	drop table SpecimensV1_Snapshot;
+go
+
+select *
+into SpecimensV1_Snapshot
+from SpecimensV1;
+
+select coalesce(ProtocolCategory, '(Unmapped)') as ProtocolCategory, sum(SpecimenCount)
+from SpecimensV1_Snapshot
+group by ProtocolCategory
 order by 1;
